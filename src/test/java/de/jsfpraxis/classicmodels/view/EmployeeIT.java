a03530ddbc8f045faef1cd5b9eb662737396ed5f
Bernd Mueller <bernd.mueller@ostfalia.de>
2020-08-26 14:30:38 +0200

    kleine Refactorings

--- src/test/java/de/jsfpraxis/classicmodels/view/EmployeeIT.java
+++ src/test/java/de/jsfpraxis/classicmodels/view/EmployeeIT.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.net.URL;
 
 import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.jboss.arquillian.graphene.Graphene;
 import org.jboss.arquillian.junit.Arquillian;
@@ -12,7 +11,6 @@ import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.Filters;
 import org.jboss.shrinkwrap.api.GenericArchive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.importer.ExplodedImporter;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Assert;
@@ -22,9 +20,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
-
-import de.jsfpraxis.classicmodels.business.EntityService;
-import de.jsfpraxis.classicmodels.business.GenericEntity;
 
 /**
  * Test für /admin/employee.xhtml
@@ -40,23 +35,13 @@ public class EmployeeIT {
 	@Deployment(testable = false)
 	public static WebArchive createDeployment() {
 		return ShrinkWrap.create(WebArchive.class)
-        		.addClass(EntityService.class)
-        		.addClass(GenericEntity.class)
-        		.addPackages(true, "de.jsfpraxis.classicmodels.business.offices")
-        		.addPackages(true, "de.jsfpraxis.classicmodels.business.accounting")
-        		.addPackages(true, "de.jsfpraxis.classicmodels.business.products")	
+				.addPackages(true, "de.jsfpraxis.classicmodels")
+				.deleteClass(OrderDetailsController.class) // wg PDFBox-Abhängigkeit
                 .addAsResource("META-INF/persistence.xml")
                 .addAsResource("import.sql")
-				.addClass(EmployeeController.class)
-				.addClass(EmployeeConverter.class)
-				.addClass(OfficeConverter.class)
-				.addClass(OfficesController.class) // für <f:selectItems>
-				.addClass(EmployeesController.class) // für <f:selectItems>
 				.merge(getWebAppArchive(), "/", Filters.includeAll())
-				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
 				.addAsWebInfResource(new File("src/test/resources/WEB-INF/web.xml"), "web.xml")
 				.addAsWebInfResource(new File("src/test/resources/WEB-INF/jboss-web.xml"), "jboss-web.xml");
-				//.addAsManifestResource(EmptyAsset.INSTANCE, "web.xml");
 	}
 	
 	
@@ -71,7 +56,7 @@ public class EmployeeIT {
 	URL deploymentUrl;
 	
 	@Test
-	@RunAsClient
+	//@RunAsClient
 	public void newEmployee() {
 	    browser.get(deploymentUrl.toExternalForm() + "admin/employee.xhtml");
 	    WebElement firstName = browser.findElement(By.id("form:firstname"));
--- src/test/java/de/jsfpraxis/classicmodels/view/EmployeeITSelenium.java
+++ src/test/java/de/jsfpraxis/classicmodels/view/EmployeeITSelenium.java
@@ -4,13 +4,11 @@ import java.io.File;
 import java.net.URL;
 
 import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.Filters;
 import org.jboss.shrinkwrap.api.GenericArchive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.importer.ExplodedImporter;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.After;
@@ -25,7 +23,6 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 
 /**
  * Test für /admin/employee.xhtml.
@@ -48,7 +45,6 @@ public class EmployeeITSelenium {
                 .addAsResource("META-INF/persistence.xml")
                 .addAsResource("import.sql")
 				.merge(getWebAppArchive(), "/", Filters.includeAll())
-				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
 				.addAsWebInfResource(new File("src/test/resources/WEB-INF/web.xml"), "web.xml")
 				.addAsWebInfResource(new File("src/test/resources/WEB-INF/jboss-web.xml"), "jboss-web.xml");
 	}
@@ -64,7 +60,7 @@ public class EmployeeITSelenium {
 	
 	
 	@Test
-	@RunAsClient
+	//@RunAsClient
 	public void newEmployee() {
 	    driver.get(deploymentUrl.toExternalForm() + "admin/employee.xhtml");
 	    WebElement firstName = driver.findElement(By.id("form:firstname"));
